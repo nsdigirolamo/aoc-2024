006562fe091aa257995b9eaf957527b3a35dd7fb
Nicholas DiGirolamo <nsdigirolamo@gmail.com>
2024-12-04 20:17:16 -0500

    Day 1 complete for real this time.

--- day1/day1.py
+++ day1/day1.py
@@ -1,26 +1,32 @@
 def read_pairs(filename: str) -> tuple[list[int], list[int]]:
     left = []
     right = []
-
     with open(filename, 'r') as file:
         for line in file:
             num1, num2 = map(int, line.strip().split())
             left.append(num1)
             right.append(num2)
-
     return left, right
 
-def main(left: list[int], right: list[int]) -> int:
+def part1(left: list[int], right: list[int]) -> int:
     left.sort()
     right.sort()
     distance = 0
-
     for l, r in zip(left, right):
         distance += abs(l - r)
-
     return distance
 
+def part2(left: list[int], right: list[int]) -> int:
+    similarity = 0
+    for l in left:
+        for r in right:
+            if l == r:
+                similarity += l
+    return similarity
+
 if __name__=="__main__":
-    lists = read_pairs("./input.txt")
-    distance = main(lists[0], lists[1])
+    left, right = read_pairs("./input.txt")
+    distance = part1(left, right)
     print(distance)
+    similarity = part2(left, right)
+    print(similarity)

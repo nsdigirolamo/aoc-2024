a67aa207387496a3ed3fd3dfc9eba9da5c8ab281
Nicholas DiGirolamo <nsdigirolamo@gmail.com>
2024-12-05 20:28:47 -0500

    Part 2 done. Really not happy with how long this took.

--- day3/day3.py
+++ day3/day3.py
@@ -15,6 +15,32 @@ def part1(program: str) -> int:
         result += lhs * rhs
     return result
 
+def part2(program: str) -> int:
+    # I was going to try to use fancy regex to parse the entire thing all at
+    # once but I gave up because I'm bad at regex.
+    result = 0
+    mul = r"mul\((\d{1,3}),(\d{1,3})\)"
+    do = r"do\(\)"
+    dont = r"don't\(\)"
+    do_flag = True
+    index = 0
+    while index < len(program):
+        # The longest an instruction can be is mul(000,000) so look ahead 12 characters.
+        look_ahead = program[index:index+12]
+        for match in re.findall(mul, look_ahead):
+            if do_flag:
+                lhs, rhs = map(int, match)
+                result += lhs * rhs
+                index += 5 # Jump ahead 5 to prevent double-count
+
+        if re.match(dont, look_ahead):
+            do_flag = False
+        elif re.match(do, look_ahead):
+            do_flag = True
+        index += 1
+
+    return result
+
 if __name__=="__main__":
     program = read_program("input.txt")
     result = part1(program)

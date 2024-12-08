9840195c1c776caa64e8f19c37fba4111ca9f6cf
Nicholas DiGirolamo <nsdigirolamo@gmail.com>
2024-12-08 14:23:49 -0500

    Part two complete. Today was easy!

--- day8/day8.py
+++ day8/day8.py
@@ -9,7 +9,7 @@ def read_locations(filename: str) -> list[list[str]]:
 
     return locations
 
-def mark_antinodes(locations: list[list[str]]) -> list[list[str]]:
+def map_frequencies(location: list[list[str]]) -> dict[str, list[tuple[int, int]]]:
     row_count, col_count = len(locations), len(locations[0])
 
     # Dictionary that maps frequencies to a list of antenna locations.
@@ -23,11 +23,16 @@ def mark_antinodes(locations: list[list[str]]) -> list[list[str]]:
                     frequencies[location] = []
                 frequencies[location].append((row, col))
 
+    return frequencies
+
+def mark_antinodes(locations: list[list[str]]) -> list[list[str]]:
+    row_count, col_count = len(locations), len(locations[0])
+    frequencies = map_frequencies(locations)
+
     # Antinodes list will store all unique locations of antinodes.
     antinodes = [ ['.'] * col_count for _ in range(row_count) ]
     for _, locations in frequencies.items():
-        # For every location pair, we're going to check if the antinode
-        # is in bounds.
+        # For every location pair, we check if the antinode is in bounds.
         for index, current_location in enumerate(locations):
             other_locations = locations[:index] + locations[index + 1:]
             for other_location in other_locations:
@@ -46,6 +51,35 @@ def mark_antinodes(locations: list[list[str]]) -> list[list[str]]:
 
     return antinodes
 
+def mark_antinodes_with_harmonics(locations: list[list[str]]) -> list[list[str]]:
+    row_count, col_count = len(locations), len(locations[0])
+    frequencies = map_frequencies(locations)
+
+    # Antinodes list will store all unique locations of antinodes.
+    antinodes = [ ['.'] * col_count for _ in range(row_count) ]
+    for _, locations in frequencies.items():
+        # For every location pair, we check if the antinode is in bounds.
+        for index, current_location in enumerate(locations):
+            other_locations = locations[:index] + locations[index + 1:]
+            for other_location in other_locations:
+                # First get the offset between the two locations
+                row_offset = current_location[0] - other_location[0]
+                col_offset = current_location[1] - other_location[1]
+                # Now just keep adding until the antinode is out of bounds.
+                antinode_row = current_location[0]
+                antinode_col = current_location[1]
+                multiplier = 0
+                while (
+                    (0 <= antinode_row and antinode_row < row_count) and
+                    (0 <= antinode_col and antinode_col < col_count)
+                ):
+                    antinodes[antinode_row][antinode_col] = '#'
+                    multiplier += 1
+                    antinode_row = current_location[0] + row_offset * multiplier
+                    antinode_col = current_location[1] + col_offset * multiplier
+
+    return antinodes
+
 def count_antinodes(antinodes: list[list[str]]) -> int:
     count = 0
     for row in antinodes:
@@ -61,7 +95,16 @@ def part_one(locations: list[list[str]]) -> int:
 
     return count
 
+def part_two(locations: list[list[str]]) -> int:
+    antinodes = mark_antinodes_with_harmonics(locations)
+    count = count_antinodes(antinodes)
+
+    return count
+
 if __name__ == "__main__":
     locations = read_locations("input.txt")
     result = part_one(locations)
+    print(result)
+    locations = read_locations("input.txt")
+    result = part_two(locations)
     print(result)

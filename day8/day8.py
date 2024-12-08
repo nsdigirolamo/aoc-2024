82b4a2fc860410b45148569597400bf67f6c759a
Nicholas DiGirolamo <nsdigirolamo@gmail.com>
2024-12-08 14:15:12 -0500

    Part one complete.

---
+++ day8/day8.py
@@ -0,0 +1,67 @@
+def read_locations(filename: str) -> list[list[str]]:
+    locations = []
+    with open(filename, 'r') as file:
+        for line in file:
+            row = []
+            for char in line.strip():
+                row.append(char)
+            locations.append(row)
+
+    return locations
+
+def mark_antinodes(locations: list[list[str]]) -> list[list[str]]:
+    row_count, col_count = len(locations), len(locations[0])
+
+    # Dictionary that maps frequencies to a list of antenna locations.
+    frequencies: dict[str, list[tuple[int, int]]] = {}
+    for row in range(row_count):
+        for col in range(col_count):
+            location = locations[row][col]
+            # If the location has an antenna, add it to the frequency dict.
+            if location != '.':
+                if location not in frequencies:
+                    frequencies[location] = []
+                frequencies[location].append((row, col))
+
+    # Antinodes list will store all unique locations of antinodes.
+    antinodes = [ ['.'] * col_count for _ in range(row_count) ]
+    for _, locations in frequencies.items():
+        # For every location pair, we're going to check if the antinode
+        # is in bounds.
+        for index, current_location in enumerate(locations):
+            other_locations = locations[:index] + locations[index + 1:]
+            for other_location in other_locations:
+                # First get the offset between the two locations
+                row_offset = current_location[0] - other_location[0]
+                col_offset = current_location[1] - other_location[1]
+                # Then add to current location to find possible antinode
+                antinode_row = current_location[0] + row_offset
+                antinode_col = current_location[1] + col_offset
+                # If the antinode isn't out of bounds, add it to the list.
+                if (
+                    (0 <= antinode_row and antinode_row < row_count) and
+                    (0 <= antinode_col and antinode_col < col_count)
+                ):
+                    antinodes[antinode_row][antinode_col] = '#'
+
+    return antinodes
+
+def count_antinodes(antinodes: list[list[str]]) -> int:
+    count = 0
+    for row in antinodes:
+        for location in row:
+            if location == '#':
+                count += 1
+
+    return count
+
+def part_one(locations: list[list[str]]) -> int:
+    antinodes = mark_antinodes(locations)
+    count = count_antinodes(antinodes)
+
+    return count
+
+if __name__ == "__main__":
+    locations = read_locations("input.txt")
+    result = part_one(locations)
+    print(result)

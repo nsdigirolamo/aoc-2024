2c6a3d94a15011f54d73a73036191b0b818f8225
Nicholas DiGirolamo <nsdigirolamo@gmail.com>
2024-12-10 19:27:56 -0500

    Part two complete! Feeling good, got it first try again.

--- day10/day10.py
+++ day10/day10.py
@@ -63,6 +63,45 @@ def search(topography: list[list[int]], trailhead: tuple[int, int]) -> set[tuple
 
     return peaks
 
+# topography: 2D list of ints (input)
+# trailhead: starting point of recursive search
+# returns: the set of already-visited peaks
+def rated_search(topography: list[list[int]], trailhead: tuple[int, int]) -> int:
+    row_count, col_count = len(topography), len(topography[0])
+    row, col = trailhead
+    current_elevation = topography[row][col]
+
+    if current_elevation == 9:
+        return 1
+
+    count = 0
+
+    # top
+    if 0 <= row - 1:
+        next_elevation = topography[row - 1][col]
+        if next_elevation == current_elevation + 1:
+            count += rated_search(topography, (row - 1, col))
+
+    # bottom
+    if row + 1 < row_count:
+        next_elevation = topography[row + 1][col]
+        if next_elevation == current_elevation + 1:
+            count += rated_search(topography, (row + 1, col))
+
+    # left
+    if 0 <= col - 1:
+        next_elevation = topography[row][col - 1]
+        if next_elevation == current_elevation + 1:
+            count += rated_search(topography, (row, col - 1))
+
+    # right
+    if col + 1 < col_count:
+        next_elevation = topography[row][col + 1]
+        if next_elevation == current_elevation + 1:
+            count += rated_search(topography, (row, col + 1))
+
+    return count
+
 def part_one(topography: list[list[int]]) -> int:
     trailheads = find_trailheads(topography)
     count = 0
@@ -72,7 +111,19 @@ def part_one(topography: list[list[int]]) -> int:
 
     return count
 
+def part_two(topography: list[list[int]]) -> int:
+    trailheads = find_trailheads(topography)
+    total_rating = 0
+    for trailhead in trailheads:
+        rating = rated_search(topography, trailhead)
+        total_rating += rating
+
+    return total_rating
+
 if __name__ == "__main__":
     topography = read_topography("input.txt")
     result = part_one(topography)
+    print(result)
+    topography = read_topography("input.txt")
+    result = part_two(topography)
     print(result)

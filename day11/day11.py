7c623245937993e96abda00a95f417142ed52665
Nicholas DiGirolamo <nsdigirolamo@gmail.com>
2024-12-11 20:38:23 -0500

    That's a cool optimization!

--- day11/day11.py
+++ day11/day11.py
@@ -28,6 +28,38 @@ def blink(stones: list[int]) -> list[int]:
 
     return stones
 
+def blink_optimized(stones: dict[int, int]) -> dict[int, int]:
+    new_stones = {}
+
+    for stone, count in stones.items():
+        if stone == 0:
+            if 1 in new_stones:
+                new_stones[1] = new_stones[1] + count
+            else:
+                new_stones[1] = count
+        elif len(str(stone)) % 2 == 0:
+            str_stone = str(stone)
+            halfway = len(str_stone) // 2
+
+            first_half = int(str_stone[:halfway])
+            if first_half in new_stones:
+                new_stones[first_half] = new_stones[first_half] + count
+            else:
+                new_stones[first_half] = count
+
+            second_half = int(str_stone[halfway:])
+            if second_half in new_stones:
+                new_stones[second_half] = new_stones[second_half] + count
+            else:
+                new_stones[second_half] = count
+        else:
+            new_stone = stone * 2024
+            if new_stone in new_stones:
+                new_stones[new_stone] = new_stones[new_stone] + count
+            else:
+                new_stones[new_stone] = count
+
+    return new_stones
 
 def part_one(stones: list[int]) -> int:
     for _ in range(25):
@@ -35,7 +67,31 @@ def part_one(stones: list[int]) -> int:
 
     return len(stones)
 
+def part_two(stones: list[int]) -> int:
+    new_stones = {}
+
+    for stone in stones:
+        if stone not in new_stones:
+            new_stones[stone] = 1
+        else:
+            new_stones[stone] = new_stones[stone] + 1
+
+    for _ in range(75):
+        print(new_stones)
+        new_stones = blink_optimized(new_stones)
+
+    print(new_stones)
+
+    total_count = 0
+    for stone, count in new_stones.items():
+        total_count += count
+
+    return total_count
+
 if __name__ == "__main__":
     stones = read_stones("input.txt")
     result = part_one(stones)
+    print(result)
+    stones = read_stones("input.txt")
+    result = part_two(stones)
     print(result)
